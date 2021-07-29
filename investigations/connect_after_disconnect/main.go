77c8dae63e34bf866ee79377a506a0405e67968c
Kevin Albertson <kevin.albertson@mongodb.com>
2021-07-29 14:47:02 -0400

    add connect_after_disconnect

---
+++ investigations/connect_after_disconnect/main.go
@@ -0,0 +1,47 @@
+// Q: Can you reuse a disconnected client?
+// A: No
+//
+// Run with:
+// go run ./investigations/connect_after_disconnect/main.go
+//
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb://localhost:27017"
+
+func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	if err = client.Ping(context.TODO(), nil); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Ping 1")
+
+	if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	if err = client.Connect(context.TODO()); err != nil {
+		panic(err) // panics here: server is closed
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Ping 2")
+
+}
