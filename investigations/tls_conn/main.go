1039126447e29d7d5319c63275c3fbe43982ff2a
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-10 21:00:11 -0500

    add TLS connection for PyKMIP

---
+++ investigations/tls_conn/main.go
@@ -0,0 +1,40 @@
+// This was run with: go version go1.17.2 darwin/amd64
+// File name: tls_conn/main.go
+// Run with: go run ./tls_conn/main.go
+package main
+
+import (
+	"crypto/tls"
+	"log"
+)
+
+const pykmip_path = "/Users/kevin.albertson/code/PyKMIP"
+
+func main() {
+	cert, err := tls.LoadX509KeyPair(pykmip_path+"/bin/client_certificate_jane_doe.pem", pykmip_path+"/bin/client_key_jane_doe.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := tls.Dial("tcp", "localhost:5696", &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		InsecureSkipVerify: true, // Do not verify hostname or server certificate signature.
+	})
+	if err != nil {
+		panic("failed to connect: " + err.Error())
+	}
+	conn.Close()
+}
+
+// Sample output:
+//
+//   panic: failed to connect: remote error: tls: handshake failure
+//
+//   goroutine 1 [running]:
+//   main.main()
+//           /Users/kevin.albertson/code/go-bootstrap/investigations/tls_conn/main.go:23 +0x239
+//   exit status 2
+//
+// To have the TLS handshake succeed, set the following in tls.Config:
+// CipherSuites: []uint16{tls.TLS_RSA_WITH_AES_128_CBC_SHA256},
+//
