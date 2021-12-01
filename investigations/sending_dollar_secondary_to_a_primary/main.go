a8a0bf43d9932f1c35c0ad4ff63f82c36f7279c7
Kevin Albertson <kevin.albertson@mongodb.com>
2021-12-01 16:21:18 -0500

    update sending_dollary_secondary_to_a_primary

--- investigations/sending_dollar_secondary_to_a_primary/main.go
+++ investigations/sending_dollar_secondary_to_a_primary/main.go
@@ -1,3 +1,5 @@
+// Run with:
+// go run ./investigations/sending_dollar_secondary_to_a_primary/main.go
 package main
 
 import (
@@ -30,4 +32,7 @@ func main() {
 /* Outputs:
 2021/11/12 18:22:58 => CommandStarted: ping {"ping":{"$numberInt":"1"},"lsid":{"id":{"$binary":{"base64":"dvo8SfTVQzWsEJN4/cBchg==","subType":"04"}}},"$clusterTime":{"clusterTime":{"$timestamp":{"t":1636759370,"i":1}},"signature":{"hash":{"$binary":{"base64":"AAAAAAAAAAAAAAAAAAAAAAAAAAA=","subType":"00"}},"keyId":{"$numberLong":"0"}}},"$db":"admin","$readPreference":{"mode":"secondary"}}
 2021/11/12 18:22:58 <= CommandSucceeded: ping {"ok":{"$numberDouble":"1.0"},"$clusterTime":{"clusterTime":{"$timestamp":{"t":1636759370,"i":1}},"signature":{"hash":{"$binary":{"base64":"AAAAAAAAAAAAAAAAAAAAAAAAAAA=","subType":"00"}},"keyId":{"$numberLong":"0"}}},"operationTime":{"$timestamp":{"t":1636759370,"i":1}}}
+
+This was tested against a single-node replica set of the following MongoDB Server versions:
+(5.1.0-alpha-414-g69ca8b9, 5.0.2, 4.4.3, 4.2.12, 4.0.22, 3.6.21)
 */
