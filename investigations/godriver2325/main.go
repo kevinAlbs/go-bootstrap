fd5d19b18e8b922f1175a3731d0866e97befbed7
Kevin Albertson <kevin.albertson@mongodb.com>
2022-03-09 16:12:29 -0500

    add uuid-mismatch.js example

---
+++ investigations/godriver2325/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"kevinalbs.com/go-bootstrap/util"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		uri = "mongodb://localhost:27017"
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri).SetMonitor(util.CreateMonitor()))
+	if err != nil {
+		panic(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		panic(err)
+	}
+}
