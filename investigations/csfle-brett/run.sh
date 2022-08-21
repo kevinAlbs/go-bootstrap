dd5b68fd34f558cd8ca672fb2b2a65a96eedc93b
Kevin Albertson <kevin.albertson@mongodb.com>
2022-08-21 10:32:54 -0400

    add run script

---
+++ investigations/csfle-brett/run.sh
@@ -0,0 +1,3 @@
+export PKG_CONFIG_PATH="/Users/kevin.albertson/code/c-bootstrap/install/libmongocrypt-1.5.2/lib/pkgconfig"
+export DYLD_LIBRARY_PATH="/Users/kevin.albertson/code/c-bootstrap/install/libmongocrypt-1.5.2/lib/"
+go run -tags=cse ./investigations/csfle-brett/main.go
